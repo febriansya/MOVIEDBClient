09ecb3abc34e60086583de625f90550fbe66ba2c
yan febriansyah <44943550+febriansya@users.noreply.github.com>
2022-08-02 14:25:23 +0800

    sampe data

---
+++ app/src/main/java/com/example/moviedbclient/MovieList.kt
@@ -0,0 +1,15 @@
+package com.example.moviedbclient
+
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieList(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val results: List<Result>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
+)
